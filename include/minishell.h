dc4e48aee36a26be9f964d832d3aee933eccc810
Donghwi Kim <donghwi2@c2r4s2.42gyeongsan.kr>
2024-11-23 21:55:52 +0900

    tokenize 이전 split처리 완료

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: donghwi2 <donghwi2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:41:07 by donghwi2          #+#    #+#             */
-/*   Updated: 2024/11/20 16:59:44 by donghwi2         ###   ########.fr       */
+/*   Updated: 2024/11/23 21:40:51 by donghwi2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,12 @@ typedef struct s_tokenizer //토큰화 과정에서 사용할 구조체
 {
 	char	**toks;
 	char	curr_tok[1024];
-	int		tok_i;
-	int		char_i;
+	int		tok_i;//생성된 토큰인덱스 추적; toks에서 현재까지 저장된 토큰갯수 나타냄
+	int		char_i;//현재 처리중인 토큰(curr_tok)내부에서 문자위치 추적
 	int		one_qut;//single_quote
 	int		two_qut;//double_quote
 	char	c;
+	char	next_c;
 }t_tokenizer;
 
 typedef struct s_env //환경변수 -> 완벽하지 않은(VALUE값 없는) 변수 빼기
@@ -68,6 +69,9 @@ typedef struct s_sh //통합(mini"sh"ell)구조체
 	t_cmd				cmd;
 }t_sh;
 
+//error.c
+void		print_error_and_exit(char *err_msg);
+
 //main.c
 void		init_sh_list(t_sh *sh_list);
 
@@ -88,11 +92,12 @@ void		sig_handler(int sig);
 void		sig_handle(t_sh *sh_list);
 
 //tokenize_input.c
+int			check_quote_num(char *input);
 void		tokenize_input(char *input, t_sh *sh_list);
 
 //tokenize_split.c
 char**		tokenize_split(char* input, int* token_count);
-void free_tokens(char** toks, int tok_count);
+void		free_tokens(char** toks, int tok_count);
 
 //util.c
 int			ft_strcmp(const char *s1, const char *s2);
--- source/tokenize_split.c
+++ source/tokenize_split.c
@@ -6,19 +6,31 @@
 /*   By: donghwi2 <donghwi2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 11:24:16 by donghwi2          #+#    #+#             */
-/*   Updated: 2024/11/20 16:41:59 by donghwi2         ###   ########.fr       */
+/*   Updated: 2024/11/23 21:43:41 by donghwi2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+// 동적 할당된 토큰 메모리 해제
+void	free_tokens(char** toks, int tok_count)
+{
+	int i = 0;
+	while (i < tok_count)
+	{
+		free(toks[i]);
+		i++;
+	}
+	free(toks);
+}
+
 // 현재 토큰을 tokens에 추가하는 함수
-void add_token(t_tokenizer *tok)
+void	add_token(t_tokenizer *tok)
 {
 	if (tok->char_i > 0)
 	{
 		tok->curr_tok[tok->char_i] = '\0';
-		tok->toks[(tok->tok_i)++] = ft_strdup(tok->curr_tok);
+		tok->toks[(tok->tok_i)++] = ft_strdup(tok->curr_tok);//할당
 		tok->char_i = 0;
 	}
 }
@@ -28,7 +40,9 @@ void	init_tokenizer(t_tokenizer *tok)
 	int	i;
 
 	i = 0;
-	tok->toks = malloc(sizeof(char *) * 128);
+	tok->toks = malloc(sizeof(char *) * 128);//할당
+	if (tok->toks == NULL)
+		exit(1);
 	while (tok->curr_tok[i] != 0)
 	{
 		tok->curr_tok[i] = '\0';
@@ -39,9 +53,26 @@ void	init_tokenizer(t_tokenizer *tok)
 	tok->one_qut = 0;
 	tok->two_qut = 0;
 	tok->c = 0;
+	tok->next_c = 0;
 }
 
-void	split_if_else_part(t_tokenizer *tok)
+void	take_pipe_and_anglebracket(t_tokenizer *tok, int *i)
+{
+	add_token(tok);
+	tok->curr_tok[0] = tok->c;
+	if ((tok->c == '>' && tok->next_c == '>')\
+		|| (tok->c == '<' && tok->next_c == '<'))
+	{
+		tok->curr_tok[1] = tok->next_c;
+		tok->curr_tok[2] = '\0';
+		(*i)++;
+	}
+	else
+		tok->curr_tok[1] = '\0';
+	tok->toks[tok->tok_i++] = ft_strdup(tok->curr_tok);
+}
+
+void	split_if_else_part(t_tokenizer *tok, int *i)
 {
 	if (tok->one_qut != 0 || tok->two_qut != 0)
 	{
@@ -62,13 +93,8 @@ void	split_if_else_part(t_tokenizer *tok)
 		else
 			tok->two_qut = 1;
 	}
-	else if (ft_strchr("|<>", tok->c))
-	{
-		add_token(tok);
-		tok->curr_tok[0] = tok->c;
-		tok->curr_tok[1] = '\0';
-		tok->toks[tok->tok_i++] = ft_strdup(tok->curr_tok);
-	}
+	else if (ft_strchr("|<>", tok->c))//파이프 및 꺽쇠는 따로 처리
+		take_pipe_and_anglebracket(tok, i);
 	else
 		tok->curr_tok[tok->char_i++] = tok->c;
 }
@@ -84,22 +110,12 @@ char** tokenize_split(char* input, int* token_count)
 	while (input[i] != '\0')
 	{
 		tok.c = input[i];
-		split_if_else_part(&tok);
+		tok.next_c = input[i + 1];
+		split_if_else_part(&tok, &i);
 		i++;
 	}
 	add_token(&tok);
 	tok.toks[tok.tok_i] = NULL;
 	*token_count = tok.tok_i;
 	return (tok.toks);
-}
-// 동적 할당된 토큰 메모리 해제
-void free_tokens(char** toks, int tok_count)
-{
-	int i = 0;
-	while (i < tok_count)
-	{
-		free(toks[i]);
-		i++;
-	}
-	free(toks);
 }
